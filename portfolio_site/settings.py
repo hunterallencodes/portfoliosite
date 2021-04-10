aefd5c937097921e30be360635f0d259e83e9e17
snake-whisperer <79474323+snake-whisperer@users.noreply.github.com>
2021-04-10 16:31:24 -0700

    added comma

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
         'HOST': env("DB_HOST"),
-        'POST': env("DB_PORT")
+        'POST': env("DB_PORT"),
     }
 }
 
