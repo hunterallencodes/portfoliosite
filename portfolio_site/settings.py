90c05fdded235180128e844ac0880a86ac351728
hunterallencodes <hunterallen.codes@gmail.com>
2021-08-02 10:27:46 -0500

    Update IP address

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['18.215.62.96', 'hunterallen.codes', 'www.hunterallen.codes', 'http://hunterallen.codes', 'http://www.hunterallen.codes',]
+ALLOWED_HOSTS = ['147.182.238.202', 'hunterallen.codes', 'www.hunterallen.codes', 'http://hunterallen.codes', 'http://www.hunterallen.codes',]
 
 
 # Application definition
